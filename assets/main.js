72da5f725cabb0f4f71843c23feab04b248e303d
cohencoo <cohencoombs@outlook.com>
2023-02-09 15:29:02 +1030

    v2.4

--- assets/main.js
+++ assets/main.js
@@ -15,6 +15,13 @@ const footer = `
     <div class="contact-container">
         <div class="contact">
             <span class="box">
+                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-mail"><path d="M4 4h16c1.1 0 2 .9 2 2v12c0 1.1-.9 2-2 2H4c-1.1 0-2-.9-2-2V6c0-1.1.9-2 2-2z"></path><polyline points="22,6 12,13 2,6"></polyline></svg>
+                Email:
+            </span>
+            <a href="mailto:mailto:wbusch@suvcwmo.org">wbusch@suvcwmo.org</a>
+        </div>
+        <div class="contact">
+            <span class="box">
                 <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-facebook"><path d="M18 2h-3a5 5 0 0 0-5 5v3H7v4h3v8h4v-8h3l1-4h-4V7a1 1 0 0 1 1-1h3z"></path></svg>
                 Facebook: 
             </span>
