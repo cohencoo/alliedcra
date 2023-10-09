03620117fcee4568f6c7d55ed3db0f2fed134254
Cohen <69182766+cohencoo@users.noreply.github.com>
2023-10-09 14:18:00 +1030

    Update main.js

--- assets/main.js
+++ assets/main.js
@@ -15,12 +15,6 @@ const footer = `
     <div class="contact-container">
         <div class="contact">
             <span class="box">
-                Phone:
-            </span>
-            <a href="tel:2605573616">260-557-3616</a>
-        </div>
-        <div class="contact">
-            <span class="box">
                 <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-mail"><path d="M4 4h16c1.1 0 2 .9 2 2v12c0 1.1-.9 2-2 2H4c-1.1 0-2-.9-2-2V6c0-1.1.9-2 2-2z"></path><polyline points="22,6 12,13 2,6"></polyline></svg>
                 Email:
             </span>
